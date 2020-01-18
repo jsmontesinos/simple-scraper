0f1c16b683e6883925b76ea03137603ad74824f3
Juan Sánchez <jsmontesinos@gmail.com>
2020-01-18 19:35:20 +0100

    Example passing an optional click parameter and scrapping an element

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://example.com');
+  await page.goto(argv.url);
+  const result = await page.evaluate(({ clickQuery, query }) => {
+      if (clickQuery) {
+        document.querySelector(clickQuery).click();
+      }
+      return document.querySelector(query).innerText;
+  }, { click: argv.clickQuery, query: argv.query });
   await page.screenshot({path: 'example.png'});
-
+  console.log(result);
   await browser.close();
 })();
